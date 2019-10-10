5c08e6dece0c1884ec988daa0b7005c4fa08583e
Tomi Virtanen <tltv@vaadin.com>
2019-10-10 11:10:51 +0300

    Changes for version 2.0.0 for Vaadin 14

--- noscroll-grid/src/main/java/org/vaadin/componentfactory/NoScrollGrid.java
+++ noscroll-grid/src/main/java/org/vaadin/componentfactory/NoScrollGrid.java
@@ -58,7 +58,7 @@ import com.vaadin.pro.licensechecker.LicenseChecker;
 @JsModule("./noscroll-grid.js")
 public class NoScrollGrid<T> extends Grid<T> {
 
-	private static String PROJECT_VERSION = "1.0.1";
+	private static String PROJECT_VERSION = "2.0.0";
     private static String PROJECT_NAME = "vcf-noscroll-grid";
     
 	private Registration dataProviderListener;
